b22e2e51d7d017733dd3ace7a8988c979f894ac0
Emmanuel Babatunde <iambabatundes@gmail.com>
2023-06-09 02:45:04 -0700

    Creating a comment system

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Newsletter() {
+  return (
+    <section className="newsletter">
+      <div className="newsletter__main">
+        <article>
+          <h1>Subscribe to our Newsletter</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos, voluptates.
+          </p>
+        </article>
+
+        <section></section>
+      </div>
+    </section>
+  );
+}
